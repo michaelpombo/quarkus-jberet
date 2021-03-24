55c37bbc16a29afc40d700d045d8ce7882093c17
Michael Pombo <michael.pombo@gmail.com>
2021-03-24 18:06:25 -0300

    uses 'test' properties when in test mode

--- rest/runtime/src/main/java/io/quarkiverse/jberet/rest/runtime/JBeretRestProducer.java
+++ rest/runtime/src/main/java/io/quarkiverse/jberet/rest/runtime/JBeretRestProducer.java
@@ -7,12 +7,19 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jberet.rest.client.BatchClient;
 
 import io.quarkus.arc.DefaultBean;
+import io.quarkus.runtime.LaunchMode;
 
 public class JBeretRestProducer {
     @ConfigProperty(name = "quarkus.http.host")
     private String host;
     @ConfigProperty(name = "quarkus.http.port")
     private int port;
+    @ConfigProperty(name = "quarkus.http.ssl-port")
+    private int sslPort;
+    @ConfigProperty(name = "quarkus.http.test-port")
+    private int testPort;
+    @ConfigProperty(name = "quarkus.http.test-ssl-port")
+    private int testSllPort;
     @ConfigProperty(name = "quarkus.http.insecure-requests")
     private String insecure;
 
@@ -20,7 +27,28 @@ public class JBeretRestProducer {
     @DefaultBean
     @Singleton
     public BatchClient batchClient() {
-        final String scheme = "enabled".equals(insecure) ? "http" : "https";
-        return new BatchClient(scheme + "://" + host + ":" + port);
+        final String scheme = isInsecurityEnabled() ? "http" : "https";
+        return new BatchClient(scheme + "://" + host + ":" + resolvePort());
+    }
+
+    private int resolvePort() {
+        if (isInsecurityEnabled()) {
+            if (LaunchMode.current() == LaunchMode.TEST) {
+                return testPort;
+            } else {
+                return port;
+            }
+        } else {
+            if (LaunchMode.current() == LaunchMode.TEST) {
+                return testSllPort;
+            } else {
+                return sslPort;
+            }
+        }
+
+    }
+
+    private boolean isInsecurityEnabled() {
+        return "enabled".equals(insecure);
     }
 }
